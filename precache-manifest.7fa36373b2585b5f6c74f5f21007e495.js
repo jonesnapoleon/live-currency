4e585fbc93924033fe38c08b41d5b40a1d0cb0c9
Jones Napoleon <jonesnapoleonautumn1@gmail.com>
2020-10-03 01:12:52 +0700

    Updates

---
+++ precache-manifest.7fa36373b2585b5f6c74f5f21007e495.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48f8b17a7112d0202da0e7c2e3fb2893",
+    "url": "/live-currency/index.html"
+  },
+  {
+    "revision": "a612d41d1acc7081d6c9",
+    "url": "/live-currency/static/css/main.9fce4171.chunk.css"
+  },
+  {
+    "revision": "77a9c5c86387299377a4",
+    "url": "/live-currency/static/js/2.679b9927.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/live-currency/static/js/2.679b9927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a612d41d1acc7081d6c9",
+    "url": "/live-currency/static/js/main.a712fdee.chunk.js"
+  },
+  {
+    "revision": "5b1a65dc079ec45e6c9a",
+    "url": "/live-currency/static/js/runtime-main.33a55b61.js"
+  }
+]);

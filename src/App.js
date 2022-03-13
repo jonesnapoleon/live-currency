7cb6a9521e21dff122b6e679a43009a2a03b3441
Jones Napoleon Autumn <13518086@std.stei.itb.ac.id>
2022-03-13 19:45:07 +0700

    fix meta refresh html

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { CurrencyProvider } from "./context/CurrencyContext";
 import { PriceProvider } from "./context/PriceContext";
 import { AlertProvider } from "./context/AlertContext";
 import Landing from "./pages/Landing";
 import LargeScreen from "./pages/LargeScreen";
 import { useWidth } from "./utils/hooks";
-import ReactGA from "react-ga";
 import "./App.css";
 
 const App = () => {
   const { isMobile } = useWidth();
-  useEffect(() => {
-    ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID, {
-      debug: true,
-      titleCase: true,
-    });
-  }, []);
 
   return (
     <AlertProvider>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+import ReactGA from "react-ga";
+
+ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID, {
+  debug: true,
+  titleCase: true,
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <App />

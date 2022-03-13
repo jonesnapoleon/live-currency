b49e3fe1cc718d45b126d21d06886e7b5a8412b3
Jones Napoleon Autumn <13518086@std.stei.itb.ac.id>
2022-03-13 19:27:55 +0700

    add react-ga

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ import { AlertProvider } from "./context/AlertContext";
 import Landing from "./pages/Landing";
 import LargeScreen from "./pages/LargeScreen";
 import { useWidth } from "./utils/hooks";
+import ReactGA from "react-ga";
 import "./App.css";
 
 const App = () => {
   const { isMobile } = useWidth();
+  ReactGA.initialize("G-TBFW4LY2KP", {
+    debug: true,
+    titleCase: false,
+    gaOptions: {
+      userId: 123,
+    },
+  });
   return (
     <AlertProvider>
       <PriceProvider>
